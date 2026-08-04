6e442dfccb8b062b0a49029f48a36fb1b441c9f3
Glenn Wallace <glennw-coding@proton.me>
2026-08-05 00:17:48 +0200

    operators + while loops

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,13 @@
 use chumsky::Parser;
 
 fn main() {
-    let crisp_txt = "(def f (fn [x:i32, y:i32]->i32 10))";
+    let crisp_txt = "\
+        (\
+            (def f (fn [x:i32, y:i32] -> i32\
+                (return 45)\
+            ))\
+            (loop (x < 1) (+ 2 3))\
+        )";
+    println!("{:#?}", crisp_txt);
     println!("{:#?}", crisp::crip_parser().parse(crisp_txt));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,27 @@
+/// - [x] Variables
+/// - [x] Functions
+/// - [ ] Loops (while)
 pub mod ast;
-
+use crate::ast::{Expr, Literal, Param, Type};
 use chumsky::prelude::*;
 
-use crate::ast::{Expr, Literal, Param, Type};
-
 pub fn crip_parser<'a>() -> impl Parser<'a, &'a str, Vec<Expr>> {
+    // parse expressions
     let expr = recursive(|expr| {
+        // reserved keywords
         let reserved = [
-            "def", "fn", "i32", "i64", "f32", "f64", "u32", "u64", "bool",
+            "def", "fn", "i32", "i64", "f32", "f64", "u32", "u64", "bool", "loop",
         ];
+        // built-in operators
+        let operator = one_of("+-*/=<>!&|")
+            .repeated()
+            .at_least(1)
+            .collect::<String>(); // generic identifiers (function/variable names and the like)
         let identifier = text::ident()
             .map(String::from)
+            .or(operator)
             .filter(move |s| !reserved.contains(&s.as_str()))
-            .map(Expr::Identifier);
+            .map(Expr::Identifier); // text or numerical data typed in at comptime
         let literal = {
             let string = just('"')
                 .ignore_then(none_of("\"").repeated().collect::<String>())
@@ -32,6 +41,7 @@ pub fn crip_parser<'a>() -> impl Parser<'a, &'a str, Vec<Expr>> {
             };
             choice((string, number))
         };
+        // keywords referring to types (type hints etc)
         let type_keyword = choice((
             just("i32").to(Type::I32),
             just("i64").to(Type::I64),
@@ -41,11 +51,15 @@ pub fn crip_parser<'a>() -> impl Parser<'a, &'a str, Vec<Expr>> {
             just("u64").to(Type::U64),
             just("bool").to(Type::Bool),
         ));
+        // specific type hint form (: i32)
         let type_annotation = just(':').padded().ignore_then(type_keyword.clone());
+        // identifier with a type hint (abc: i32)
         let typed_identifier = text::ident()
             .map(String::from)
             .then(type_annotation.clone());
+        // syntactic forms with specific functionality
         let special_forms = {
+            // using the def keywords to define a new variable or function
             let define = just("def")
                 .padded()
                 .ignore_then(text::ident().map(String::from))
@@ -57,6 +71,7 @@ pub fn crip_parser<'a>() -> impl Parser<'a, &'a str, Vec<Expr>> {
                     type_annotation,
                     value: Box::new(value),
                 });
+            // parameter list for functions; basically a bunch of annotated identifiers
             let func_params = typed_identifier
                 .padded()
                 .separated_by(just(',').padded())
@@ -71,7 +86,9 @@ pub fn crip_parser<'a>() -> impl Parser<'a, &'a str, Vec<Expr>> {
                         })
                         .collect::<Vec<Param>>()
                 });
+            // function return type, self explanatory
             let func_return_type = just("->").padded().ignore_then(type_keyword.clone());
+            // annoymous function definitions
             let func = just("fn")
                 .padded()
                 .ignore_then(func_params)
@@ -83,8 +100,19 @@ pub fn crip_parser<'a>() -> impl Parser<'a, &'a str, Vec<Expr>> {
                     return_type,
                     body: Box::new(value),
                 });
-            choice((define, func))
+            // while condition is true do body
+            let while_loop = just("loop")
+                .padded()
+                .ignore_then(expr.clone())
+                .then(expr.clone())
+                .delimited_by(just('(').padded(), just(")").padded())
+                .map(|(condition, body)| Expr::Loop {
+                    condition: Box::new(condition),
+                    body: Box::new(body),
+                });
+            choice((define, func, while_loop))
         };
+        // creation of a new scope; important for certain special forms
         let block = expr
             .clone()
             .repeated()
